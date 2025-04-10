3b6f710f974e1e089801b8e9f30f8e43e2b0f56c
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2025-04-10 14:10:38 +0000

    faire la 3eme section mais il existe des probleme

--- BacPlus/resources/views/Pages/welcome.blade.php
+++ BacPlus/resources/views/Pages/welcome.blade.php
@@ -2,29 +2,29 @@
 @section('content')
     <section class="w-[93%] flex flex-row justify-self-center p-4">
         <div class="w-[50%] flex flex-col ">
-        <div class=" flex justify-center items-center flex-col gap-4">
-            <p class="text-6xl" style="font-family: 'Mirza' ,Serif ;">
-                Votre futur commence ici :<br> découvrez les meilleures écoles pour vous.
-            </p>
-            <div class="w-[70%] py-6  ">
-{{--                <button class=" w-full flex justify-center p-4 text-3xl my-4  ">--}}
-{{--                <span>Decouvert les Ecoles</span>--}}
-{{--                </button>--}}
-{{--                <button class=" w-full flex justify-center p-4 text-3xl  ">--}}
-{{--                <span>Dernieres Nouvelle</span>--}}
-{{--                </button>--}}
-                <x-button >Decouvert les Ecoles</x-button>
-                <x-button >Dernieres Nouvelle</x-button>
+            <div class=" flex justify-center items-center flex-col gap-4">
+                <p class="text-6xl" style="font-family: 'Mirza' ,Serif ;">
+                    Votre futur commence ici :<br> découvrez les meilleures écoles pour vous.
+                </p>
+                <div class="w-[70%] py-6  ">
+                    {{--                <button class=" w-full flex justify-center p-4 text-3xl my-4  ">--}}
+                    {{--                <span>Decouvert les Ecoles</span>--}}
+                    {{--                </button>--}}
+                    {{--                <button class=" w-full flex justify-center p-4 text-3xl  ">--}}
+                    {{--                <span>Dernieres Nouvelle</span>--}}
+                    {{--                </button>--}}
+                    <x-button>Decouvert les Ecoles</x-button>
+                    <x-button>Dernieres Nouvelle</x-button>
 
+                </div>
             </div>
-        </div>
         </div>
         <div class="h-fit ">
             <img src="{{asset('../images/HomeFront.svg')}}" alt="hello">
         </div>
     </section>
     <section class="w-full h-content bg-white flex flex-row p-12  ">
-        <div class="w-[60%] h-fit justify-center justify-self-center items-center">
+        <div class="w-[60%] h-fit justify-center justify-self-center items-center ">
       <span class="text-3xl text-center font-serif" style="font-family: 'Shanti', Serif;">
   <p class="mb-4">143 établissements publics d’enseignement supérieur t’attendent au Maroc...</p>
 
@@ -32,25 +32,53 @@
 
   <p class="mb-4">Que tu rêves d’ingénierie, d’art, de médecine, d’économie ou de sciences humaines — il y a une place pour toi.</p>
 
-
   <p class="text-5xl font-bold text-[#8ebe74]">✨ Trouve ce qui t’anime. Suis ta passion. Bâtis ton futur.</p>
 </span>
+            <x-button>
+                sign up
+            </x-button>
 
         </div>
-            <div class="flex flex-col items-center  w-[40%] ">
-                <!-- Main circle with cutout -->
-                <div class=" w-64 h-64  rounded-full bg-[#8ebe74] flex items-center justify-center">
-                    <div class="text-white text-3xl font-bold z-10">
-                        12 university
-                    </div>
+        <div class="flex flex-col items-center  w-[40%] ">
+            <!-- Main circle with cutout -->
+            <div class=" w-64 h-64  rounded-full bg-[#8ebe74] flex items-center justify-center">
+                <div class="text-white text-3xl font-bold z-10">
+                    12 university
                 </div>
-                <div class=" relative left-8  top-[-12%] w-64 h-64 rounded-full bg-white flex items-center justify-center opacity-50 " ">
-                    <div class="  text-[#8ebe74] text-3xl font-bold z-10">
-                        143 etablisement
-                    </div>
+            </div>
+            <div
+                class=" relative left-8  top-[-12%] w-64 h-64 rounded-full bg-white flex items-center justify-center opacity-75 ">
+                <div class="  text-[#8ebe74] text-3xl font-bold z-10">
+                    143 etablisement
                 </div>
+            </div>
 
         </div>
+    </section>
+    <section class="w-full">
+        <div class="flex flex-col justify-center w-full">
+            <span>
+            Universites marocaines
+            </span>
+            <div class="grid grid-col-2 justify-self-center gap-2 w[90%]">
+                <div class="w-10 h-10"><img src="{{asset('Images/Ens.jpg')}}" alt=""></div>
+                <div class="w-10 h-10"><img src="{{asset('Images/Ens.jpg')}}" alt=""></div>
+                <div class="w-10 h-10"><img src="{{asset('Images/Ens.jpg')}}" alt=""></div>
+                <div class="w-10 h-10"><img src="{{asset('Images/Ens.jpg')}}" alt=""></div>
+                <div class="w-10 h-10"><img src="{{asset('Images/Ens.jpg')}}" alt=""></div>
+                <div class="w-10 h-10"><img src="{{asset('Images/Ens.jpg')}}" alt=""></div>
+                <div class="w-10 h-10"><img src="{{asset('Images/Ens.jpg')}}" alt=""></div>
+                <div class="w-10 h-10"><img src="{{asset('Images/Ens.jpg')}}" alt=""></div>
+                <div class="w-10 h-10"><img src="{{asset('Images/Ens.jpg')}}" alt=""></div>
+                <div class="w-10 h-10"><img src="{{asset('Images/Ens.jpg')}}" alt=""></div>
+                <div class="w-10 h-10"><img src="{{asset('Images/Ens.jpg')}}" alt=""></div>
+
+
+            </div>
+            <div class="grid grid-cols-2 grid-rows-2 gap-4">
+            </div>
+        </div>
+
     </section>
 
 @endsection
