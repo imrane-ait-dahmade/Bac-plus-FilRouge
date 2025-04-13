1e0bf41435f03b5bfdd2bf9e1234afa41ec417aa
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2025-04-13 09:02:52 +0000

    faire le validation de input de sing up en partie backend dans le class authantification methode register et

--- BacPlus/app/Http/Controllers/Authantification.php
+++ BacPlus/app/Http/Controllers/Authantification.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use http\Env\Response;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class Authantification extends Controller
 {
@@ -19,9 +22,28 @@ class Authantification extends Controller
      */
     public function store(Request $request)
     {
-        //
     }
 
+public function register(Request $request){
+       $requestValid = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|confirmed|min:8',
+        ]);
+       return response()->json($requestValid);
+//       if($requestValid){
+//           User::create([
+//               'name' => $requestValid['name'],
+//               'email' => $requestValid['email'],
+//               'password' => Hash::make($requestValid['password']),
+//           ]);
+//           return response()->json(['message' => 'User Created Successfully'],200);
+//       }
+//       else{
+//           return response()->json(['message' => 'User Created Failed'],400);
+//       }
+
+}
     /**
      * Display the specified resource.
      */
--- BacPlus/routes/api.php
+++ BacPlus/routes/api.php
@@ -2,8 +2,10 @@
 
 use illuminate\http\Request;
 use Illuminate\Support\Facades\Route;
+use http\Env\Response;
 
+Route::get('/register', [\App\Http\Controllers\Authantification::class, 'register']);
+route::get('/', function(){
+    return response()->json(['message' => 'User Created Failed'],200);
 
-Route:get('/', function (){
-    return response('Hello World');
 });
