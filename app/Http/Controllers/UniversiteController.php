39ab1fae07097b17caeb1326b55b2e0ebb47a3e4
imrane-ait-dahmade <imraneaitdahmade2006@gmail.com>
2025-05-05 18:13:19 +0000

    feat(universite ) create universite modal

--- app/Http/Controllers/UniversiteController.php
+++ app/Http/Controllers/UniversiteController.php
@@ -1,22 +1,17 @@
 <?php
 
-//namespace App\Http\Controllers;
-//
-//use App\Enums\Universite;
-//use Illuminate\Http\Request;
-//
-//class UniversiteController extends Controller
-//{
-//
-//    private array $Universites;
-//
-//
-//
-//    public function RecupererListeUniversite()
-//    {
-//
-//     $this->Universites = Universite::cases();
-//     return $this->Universites;
-////     dd($this->Universites);
-//    }
-//}
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UniversiteController extends Controller
+{
+
+
+
+
+
+  public function AllUniversites(){
+
+  }
+}
